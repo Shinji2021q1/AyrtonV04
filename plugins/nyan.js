40a9c8ad1fba53090e21cec736f2082ad0c83c7f
Shinji2021q1 <94997958+Shinji2021q1@users.noreply.github.com>
2022-03-13 00:40:35 -0500

    Update nyan.js

--- plugins/nyan.js
+++ plugins/nyan.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn}) => {
   let { 
 url
 } = json
-let stiker = await sticker(null, url, 'Nyan', '©The Shadow Brokers - Bot')
+let stiker = await sticker(null, url, 'Nyan', '©King Shadow Monarch - Bot')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
