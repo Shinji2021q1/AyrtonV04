2502dcbb8b412bebeafbc250052760f014b5602a
Shinji2021q1 <94997958+Shinji2021q1@users.noreply.github.com>
2022-03-13 00:43:28 -0500

    Update stonk.js

--- plugins/stonk.js
+++ plugins/stonk.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text }) => {
     let stonk = global.API('https://hardianto.xyz', '/api/stonk', {
     image: await conn.getProfilePicture(m.sender).catch(_ => ''),
     apikey: 'hardianto'})
-    let stiker = await sticker(null, stonk, 'stonks', '©The Shadow Brokers - Bot')
+    let stiker = await sticker(null, stonk, 'stonks', '©King Shadow Monarch - Bot')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
