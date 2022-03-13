6a8b24e0fe6b670e9aa3b3700cf92fe981c02d30
Shinji2021q1 <94997958+Shinji2021q1@users.noreply.github.com>
2022-03-13 00:33:53 -0500

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -23,7 +23,7 @@ let info = `
 *8.-* Proximamente
 `.trim() 
 
-conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
+conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 KING SHADOW MONARCH - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
 
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
 module.exports = handler
